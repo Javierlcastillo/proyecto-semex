aeac11c16fff6548e036ae04b12eec60632b83ff
Diego Hernandez Herrera <diegohh0411@gmail.com>
2025-09-04 16:41:04 -0600

    fix: update TrafficLight initialization and rendering to include rotation

--- defined_tlconnections.py
+++ defined_tlconnections.py
@@ -3,7 +3,7 @@ from defined_routes import routes
 
 tlconnections: list[TLConnection] = []
 
-tl1 = TrafficLight(270, 250)
+tl1 = TrafficLight(385, 160, 58)
 tlc1 = TLConnection(routes[0], tl1, 100)
 
 tlconnections.append(tlc1)
--- traffic_light.py
+++ traffic_light.py
@@ -1,7 +1,8 @@
 import enum
 from route import Route
 from matplotlib.axes import Axes
-from matplotlib.patches import Circle
+from matplotlib.patches import Rectangle
+from matplotlib.transforms import Affine2D
 from typing import Optional
 
 class TrafficLightState(enum.Enum):
@@ -15,21 +16,27 @@ class TrafficLight:
     """
 
     state: TrafficLightState
-    patch: Optional[Circle] = None
+    patch: Optional[Rectangle] = None
 
     # The universal location where the traffic light will be rendered for all routes
     x: float
     y: float
+    rotation: float = 0.0
 
-    def __init__(self, x: float, y: float):
+    def __init__(self, x: float, y: float, rotation: float = 0.0):
         self.state = TrafficLightState.RED
         self.x = x
         self.y = y
+        self.rotation = rotation
 
     def plot(self, ax: Axes):
         if not self.patch:
-            self.patch = Circle((self.x, self.y), 5, color=self.state.value.lower(), zorder=2)
-            ax.add_patch(self.patch) 
+            self.patch = Rectangle((self.x, self.y), 7, 35, color=self.state.value.lower(), edgecolor='black', zorder=2)
+
+            self.patch.set_transform(
+                Affine2D().rotate_deg_around(self.x, self.y, self.rotation) + ax.transData
+            )
+            ax.add_patch(self.patch)
         else:
             self.patch.set_color(self.state.value.lower())
 
